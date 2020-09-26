5830a1914d08c5d83f1ecf34641a782abbf1fb24
Hannah Livnat <hannahlivnat@gmail.com>
2020-09-26 16:27:34 -0700

    :green_heart: added-lint-testing

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: ["airbnb", "eslint:recommended", "plugin:react/recommended"],
+    parser: "babel-eslint",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        "import/extensions": [".js", ".jsx"],
+        "import/resolver": {
+            node: {},
+            webpack: "webpack.config.js",
+        },
+    },
+    plugins: ["react", "react-hooks"],
+    rules: {
+        "linebreak-style": "off",
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+        "arrow-parens": ["error", "as-needed"],
+        indent: [
+            "error",
+            2,
+            {
+                SwitchCase: 1,
+                MemberExpression: "off",
+                ignoredNodes: ["TemplateLiteral"],
+            },
+        ],
+        "template-curly-spacing": "off",
+    },
+};
