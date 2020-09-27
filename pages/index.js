0e16ee0bc6b448cee512933b6b9686e8b8db50ea
Hannah Livnat <hannahlivnat@gmail.com>
2020-09-27 13:40:04 -0700

    :green_heart: added scss for styling overall layout

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,10 @@ import Navigation from './components/nav'
 import Header from './components/header'
 import Main from './components/main_container'
 import Footer from './components/footer'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Ask Edd Project</title>
         <link rel="icon" href="/favicon.ico" />
