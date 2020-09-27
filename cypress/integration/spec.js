6a2bc92ecbb64217151e654cee04cfb179c04612
Hannah Livnat <hannahlivnat@gmail.com>
2020-09-27 09:47:14 -0700

    :bug: changed spec test to hello

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -6,6 +6,6 @@ context('Actions', () => {
   it('Opens automatically to help center page', () => {
 
     cy.get('.Home_container__1EcsU')
-      .should('contain', 'Welcome to Next.js!')
+      .should('contain', 'hello')
   })
 });
