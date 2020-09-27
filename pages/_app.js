5015c7468965aad7a0aca95d43cb7d75623a3b51
Hannah Livnat <hannahlivnat@gmail.com>
2020-09-26 17:02:55 -0700

    :art: fixing lint errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp() {
+  return "hello"
 }
-
-export default MyApp
