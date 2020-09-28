80577713c780b8d9ad8f035c1f6b47199e2fa7c2
Hannah Livnat <hannahlivnat@gmail.com>
2020-09-27 17:36:04 -0700

    :green_heart: added semantic ui and context api for state management / component framework

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,66 @@
 import Home from './index'
+import EDDData from '../context/Context'
 import '../styles/globals.css'
+import "semantic-ui-css/semantic.min.css"
 
 export default function MyApp() {
-  return <Home />
+  const eddData = {
+    "Unemployment Insurance" : {
+
+    },
+    "Disability Insurance" : {
+
+    },
+    "Paid Family Leave" : {
+      "Benefit Programs Online" : {
+        "General Information": {
+
+        },
+        "Registration": {
+
+        },
+        "Update Email, Password, and Security Profile": {
+
+        },
+        "Forgot Email": {
+
+        },
+        "Forgot Password": {
+
+        }
+      },
+      "SDI Online": {
+
+      },
+      "Claims Questions": {
+
+      },
+      "Payments": {
+
+      },
+    },
+    "Report Fraud": {
+
+    },
+    "Workforce Services": {
+
+    },
+    "CalJOBS": {
+
+    },
+    "Labor Market Information": {
+
+    },
+    "Taxes": {
+
+    },
+    "Benefit Overpayment": {
+
+    },
+  };
+  return (
+    <EDDData.Provider value={eddData}>
+      <Home />
+    </EDDData.Provider>
+  );
 }
