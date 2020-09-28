463081a194917c8f9c74cfb8312d36aa252ed276
Hannah Livnat <hannahlivnat@gmail.com>
2020-09-27 17:53:40 -0700

    :art: separate eddData from app

---
+++ components/edd_data.js
@@ -0,0 +1,24 @@
+const eddData = {
+  "Unemployment Insurance": {},
+  "Disability Insurance": {},
+  "Paid Family Leave": {
+    "Benefit Programs Online": {
+      "General Information": {},
+      Registration: {},
+      "Update Email, Password, and Security Profile": {},
+      "Forgot Email": {},
+      "Forgot Password": {},
+    },
+    "SDI Online": {},
+    "Claims Questions": {},
+    Payments: {},
+  },
+  "Report Fraud": {},
+  "Workforce Services": {},
+  CalJOBS: {},
+  "Labor Market Information": {},
+  Taxes: {},
+  "Benefit Overpayment": {},
+};
+
+export default eddData;
--- pages/_app.js
+++ pages/_app.js
@@ -1,66 +1,13 @@
 import Home from './index'
-import EDDData from '../context/Context'
+import Context from '../context/Context'
+import eddData from '../components/edd_data'
 import '../styles/globals.css'
 import "semantic-ui-css/semantic.min.css"
 
 export default function MyApp() {
-  const eddData = {
-    "Unemployment Insurance" : {
-
-    },
-    "Disability Insurance" : {
-
-    },
-    "Paid Family Leave" : {
-      "Benefit Programs Online" : {
-        "General Information": {
-
-        },
-        "Registration": {
-
-        },
-        "Update Email, Password, and Security Profile": {
-
-        },
-        "Forgot Email": {
-
-        },
-        "Forgot Password": {
-
-        }
-      },
-      "SDI Online": {
-
-      },
-      "Claims Questions": {
-
-      },
-      "Payments": {
-
-      },
-    },
-    "Report Fraud": {
-
-    },
-    "Workforce Services": {
-
-    },
-    "CalJOBS": {
-
-    },
-    "Labor Market Information": {
-
-    },
-    "Taxes": {
-
-    },
-    "Benefit Overpayment": {
-
-    },
-  };
   return (
-    <EDDData.Provider value={eddData}>
+    <Context.Provider value={eddData}>
       <Home />
-    </EDDData.Provider>
+    </Context.Provider>
   );
 }
