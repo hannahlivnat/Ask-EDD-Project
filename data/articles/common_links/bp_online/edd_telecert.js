53681adec991a304315f432bbb21ceb86b660100
Hannah Livnat <hannahlivnat@gmail.com>
2020-10-01 13:03:42 -0700

    :bug: fixed lint errors

--- data/articles/common_links/bp_online/edd_telecert.js
+++ data/articles/common_links/bp_online/edd_telecert.js
@@ -1,5 +1,3 @@
-import uio_edd_telecert from "../../unemployment_insurance/other/uio_what_is_edd_telecert";
-
 export default function edd_telecert() {
   return (
     <a href="http://www.edd.ca.gov/unemployment/EDD_Tele-Cert.htm">
--- data/articles/common_links/pfl_links/pfl_fact_sheet.js
+++ data/articles/common_links/pfl_links/pfl_fact_sheet.js
@@ -1,7 +1,7 @@
 export default function pfl_fact_sheet(){
   return (
-      <a href="https://edd.ca.gov/pdf_pub_ctr/de8714cf.pdf">
+    <a href="https://edd.ca.gov/pdf_pub_ctr/de8714cf.pdf">
           Paid Family Leave Fact Sheet
-      </a>
+    </a>
   );
 }
