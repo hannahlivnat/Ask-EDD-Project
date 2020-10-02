6c134a43c9b333781811601017f328d5e76ed796
utkarsh-leapfinance <utkarsh.bharti@leapfinance.com>
2020-10-02 12:00:36 +0530

    :green_heart: create nav component - Issue-2

--- components/nav.js
+++ components/nav.js
@@ -1,5 +1,33 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "../styles/navstyle.module.scss";
+import { navLinks, navRoutes } from "../constants/nav_links";
+const { HELP_CENTER } = navRoutes;
 export default function Nav() {
+  const pageUrl = useRouter(); // to use this to read route path from page url
   return (
-    <nav>nav goes here</nav>
-  )
+    <nav className={styles.navWrapper}>
+      <div className={styles.brandLogo}>
+        <img src="/assets/images/EDD.png" />
+      </div>
+      <ul>
+        {navLinks.map((_o, idx) => {
+          const { text, path, enabled } = _o;
+          return (
+            <li key={idx}>
+              <Link href={path}>
+                <a
+                  className={
+                    enabled && path == HELP_CENTER ? styles.active_link : ""
+                  }
+                >
+                  {text}
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }
